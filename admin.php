92beb07587d4f01e6f6b9d96cb253e8244a4c58d
splendid233 <18707733672@163.com>
2017-06-29 16:41:50 +0800

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+    header('Content-type:text/html;charset=utf-8');
+    date_default_timezone_set("Asia/Shanghai");
+    //url形式  index.php?controller=控制器名&method=方法名
+    session_start();
+    require_once('config.php');
+    require_once('framework/pc.php');
+    PC::run($config);
+?>
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $config = array(
+        'viewconfig'=>array(
+            'left_delimiter'=>'{',
+            'right_delimiter'=>'}',
+            'template_dir'=>'tpl',
+            'compile_dir'=>'data/template_c'
+        ),
+        'dbconfig'=>array(
+            'dbhost'=>'localhost',
+            'dbuser'=>'root',
+            'dbpsw'=>'',
+            'dbname'=>'flat_style_blog',
+            'dbcharset'=>'utf8'
+        )
+    );
+?>
